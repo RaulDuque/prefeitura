b125583dd15b956bc2e0ef5915aa690421326727
RaulDuque <raulgduque2000@gmail.com>
2021-11-25 15:04:30 -0300

    model

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class City extends Model
 {
@@ -12,4 +13,8 @@ class City extends Model
         'name',
         'state'
     ];
+    public function cityHalls(): HasMany
+    {
+        return $this->hasMany(CityHall::class);
+    }
 }
--- app/Models/CityHall.php
+++ app/Models/CityHall.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CityHall extends Model
 {
@@ -16,4 +17,8 @@ class CityHall extends Model
         'population',
         'city_id'
     ];
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
 }
