3edf1320dfdf8e8918ff83bc4d0129068ba78424
RaulDuque <raulgduque2000@gmail.com>
2021-11-28 14:07:44 -0300

    activity model e migration factory

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -5,22 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
 class Activity extends Model
 {
     use HasFactory;
-
     protected $fillable = [
-        'type',
         'time',
-        'reciprocity',
         'description',
         'pendencies',
         'status',
-        'contact_id'
+        'receptivity_id',
+        'activity_type_id',
+        'contact_id',
+
+    ];
+    protected $casts = [
+        'time' => 'datetime'
     ];
     public function contact(): BelongsTo
     {
         return $this->belongsTo(Contact::class);
     }
-
 }
---
+++ database/migrations/2021_11_27_180230_create_activities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\ActivityType;
+use App\Models\Receptivity;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('time');
+            $table->text('description')->nullable();
+            $table->text('pendencies')->nullable();
+            $table->string('status',2)->nullable();
+            $table->foreignIdFor(ActivityType::class) ->constrained() ->onDelete('restrict');
+            $table->foreignIdFor(Receptivity::class) ->constrained() ->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
