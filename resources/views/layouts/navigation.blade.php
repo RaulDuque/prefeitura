fde2c1782e4f0f840045405ba026cb284ff4e384
RaulDuque <raulgduque2000@gmail.com>
2021-11-30 17:45:43 -0300

    nova forma de navegaçao e icones

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,17 +1,14 @@
-<aside class="z-20 hidden w-64 overflow-y-auto bg-white md:block flex-shrink-0">
-    <div class="py-4 text-gray-500">
-        <a class="ml-6 text-lg font-bold text-gray-800" href="{{ route('dashboard') }}">
-            Licen
+<aside class="z-20 hidden w-64 overflow-y-auto bg-black md:block flex-shrink-0">
+    <div class="py-4 text-blue-500">
+        <a class="ml-6 text-lg font-bold text-blue" href="{{ route('dashboard') }}">
+            Licence City
         </a>
 
         <ul class="mt-6">
             <li class="relative px-6 py-3">
                 <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                     <x-slot name="icon">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                             stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
-                        </svg>
+                        <img src="https://img.icons8.com/nolan/50/control-panel.png" width="35" height="35"/>
                     </x-slot>
                     {{ __('Dashboard') }}
                 </x-nav-link>
@@ -20,13 +17,18 @@
             <li class="relative px-6 py-3">
                 <x-nav-link href="{{ route('city-halls.index') }}" :active="request()->routeIs('city-halls.*')">
                     <x-slot name="icon">
-                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        img src="https://img.icons8.com/nolan/64/city-hall.png">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
-                        </svg>
+                        <img src="https://img.icons8.com/nolan/64/city-hall.png" alt="" width="35" height="35">
                     </x-slot>
                     {{ __('Prefeituras') }}
+                </x-nav-link>
+            </li>
+
+            <li class="relative px-6 py-3">
+                <x-nav-link href="{{ route('city-halls.create') }}" :active="request()->routeIs('city-halls.create')">
+                    <x-slot name="icon">
+                        <img src="https://img.icons8.com/nolan/50/add.png" width="35" height="35"/>
+                    </x-slot>
+                    {{ __('Adicionar Prefeitura') }}
                 </x-nav-link>
             </li>
 
@@ -42,34 +44,6 @@
                 </x-nav-link>
             </li>
 
-            <li class="relative px-6 py-3">
-                <button class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                        @click="toggleMultiLevelMenu" aria-haspopup="true">
-                <span class="inline-flex items-center">
-                    <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                        <path d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
-                    </svg>
-                    <span class="ml-4">Two-level menu</span>
-                </span>
-                    <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd"
-                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                              clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-                <template x-if="isMultiLevelMenuOpen">
-                    <ul x-transition:enter="transition-all ease-in-out duration-300"
-                        x-transition:enter-start="opacity-25 max-h-0" x-transition:enter-end="opacity-100 max-h-xl"
-                        x-transition:leave="transition-all ease-in-out duration-300"
-                        x-transition:leave-start="opacity-100 max-h-xl" x-transition:leave-end="opacity-0 max-h-0"
-                        class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
-                        aria-label="submenu">
-                        <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                            <a class="w-full" href="#">Child menu</a>
-                        </li>
-                    </ul>
-                </template>
-            </li>
         </ul>
     </div>
 </aside>
