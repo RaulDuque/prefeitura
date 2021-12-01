aeb9a631709db3375621ca358444fb3e454d5616
RaulDuque <raulgduque2000@gmail.com>
2021-12-01 20:42:30 -0300

    update button edlit cityhall

--- resources/views/city-halls/edit.blade.php
+++ resources/views/city-halls/edit.blade.php
@@ -8,11 +8,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <a href="{{ route('city-halls.index') }}" class="text-indigo-600 hover:text-indigo-900">
-                        <h3 class="text-lg font-medium text-gray-900">
-                            {{ __('Voltar') }}
-                        </h3>
-                    </a>
 
                         @if ($errors->any())
                             <ul>
@@ -50,8 +45,10 @@
                             </div>
                             <div class="text-right mt-6">
                                 <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-black-100 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out">
-                                {{ __('Atualizar') }}
-
+                                    {{ __('Atualizar') }}
+                                </button>
+                                <button href="{{ route('city-halls.index') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-red-600 hover:bg-black-500 focus:outline-none focus:border-black-100 focus:shadow-outline-black active:bg-black-700 transition duration-150 ease-in-out">
+                                        {{ __('Voltar') }}
                                 </button>
                             </div>
                     </form>
