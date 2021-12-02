5737e5bac07765a42db94dcfacb13315c4899929
RaulDuque <raulgduque2000@gmail.com>
2021-12-02 15:10:18 -0300

    edit cities e city halls

--- resources/views/city-halls/edit.blade.php
+++ resources/views/city-halls/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __("Editar a$cityHall->name") }}
+            {{ __("Editar a $cityHall->name") }}
         </h2>
     </x-slot>
     <div class="py-12">
