3510cfee5059470120dca5e1a43531ce78ccd73c
RaulDuque <raulgduque2000@gmail.com>
2021-12-07 17:04:03 -0300

    up testes

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -42,6 +42,7 @@
                                 </label>
                             </div>
                             <div class="col-span-6">
+
                                 <label for="city_hall_id" class="block text-sm font-medium leading-5 text-gray-700">
                                     {{ __('Cidade') }}
                                         <select id="city_hall_id" class="mt-1 form-select block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" name="city_hall_id" autocomplete="city_hall_id" autofocus>
